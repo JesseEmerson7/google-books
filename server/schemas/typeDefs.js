f40a083c0f2552baf8847b1c4a048e78269e759f
Jesse Emerson <jesseemerson7@gmail.com>
2023-06-04 22:42:22 -0400

    adding mutations to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,13 +4,35 @@ const typeDefs = gql`
     type User {
         _id: ID!
         username: String!
-        email: string!
+        email: String!
         bookCount: Int
         savedBooks: [Book]
 
     }
 
-    
+    type Book {
+        _id: ID!
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type Auth {
+        token: ID!
+        user: User 
+      }
+
+    type Query {
+        getSingleUser: User
+    }
+
+    type Mutation {
+        createUser: (username:String!, email: String!, password: String!): Auth
+        login:(email:String!, password: String!):Auth
+    }
 `
 
 
