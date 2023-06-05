1d5b4e58512c09d15dbd26a84cd2a96208b925ee
Jesse Emerson <jesseemerson7@gmail.com>
2023-06-04 22:04:19 -0400

    adding typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,18 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: string!
+        bookCount: Int
+        savedBooks: [Book]
+
+    }
+
+    
+`
+
+
+
 module.exports = {};
